bafad713672098ef42e5e2ef20f79c6dbef8f3b0
Zhao Tang <zxt@zhaotang.ca>
2019-06-06 20:27:33 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      loading: false,
+      joke: {}
+    }
+
+    this.getJoke = this.getJoke.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  getJoke() {
+    fetch("https://sv443.net/jokeapi/category/Any")
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        loading: false,
+        joke: data
+      })
+    })
+  }
+
+  handleClick() {
+    this.getJoke()
+  }
+
+  componentDidMount() {
+    this.setState({loading:true})
+    this.getJoke()
+  }
+
+  render() {
+    let joke, delivery = ''
+    if(this.state.joke.type === 'single') {
+      joke = this.state.joke.joke
+    } else if(this.state.joke.type === 'twopart') {
+      joke = this.state.joke.setup
+      delivery = this.state.joke.delivery
+    }
+
+    const text = this.state.loading ? "loading..." : joke
+
+    return (
+      <div className='jokeBox'>
+        <h1>{text}</h1>
+        <h1>{delivery}</h1>
+        <div className='btnBox'>
+          <button onClick={this.handleClick}>Random joke</button>
+        </div>
+      </div>
+    )
+  }
 }
+
 
 export default App;
